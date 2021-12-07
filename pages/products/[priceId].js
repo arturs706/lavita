e66bc1bc6284fdff13f28e2776542ebe61a25782
arturs706 <aradionovs@yahoo.com>
2021-12-07 15:50:32 +0000

    first commit

--- pages/products/[priceId].js
+++ pages/products/[priceId].js
@@ -5,6 +5,36 @@ import { addToCart } from '../../redux/reducers/cartSlice'
 import { useRouter } from "next/router"
 
 
+export default function PriceId ({ product_price }) { 
+    const counter = useSelector((state) => state.counter)
+    const dispatch = useDispatch();
+    const router = useRouter();
+    if (router.isFallback){
+        return <h1>Loading...</h1>
+    }
+    const product_declared  = product_price.product;
+
+    return (
+        
+        <div className="h-screen">
+        <div className="flex flex-col items-center">
+            <div>{product_declared.name}</div>
+            
+            <Image
+                src={product_declared.images[0]}
+                alt={product_declared.name}
+                width={400}
+                height={400}
+            />
+            <div>Cena: €{((product_price.unit_amount) / 100).toFixed(2)}</div>
+            <button onClick = {() => dispatch(addToCart(product_declared.id))}>Add</button>
+
+        </div>
+        </div>
+    )
+}
+
+
 export async function getStaticProps(context){
     const { params } = context
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
@@ -47,35 +77,6 @@ export async function getStaticPaths(){
     }
 }
 
-
-export default function PriceId ({ product_price }) { 
-    const router = useRouter();
-    if (router.isFallback){
-        return <h1>Loading...</h1>
-    }
-    const counter = useSelector((state) => state.counter)
-    const dispatch = useDispatch();
-    const product_declared  = product_price.product;
-
-    return (
-        
-        <div className="h-screen">
-        <div className="flex flex-col items-center">
-            <div>{product_declared.name}</div>
-            
-            <Image
-                src={product_declared.images[0]}
-                alt={product_declared.name}
-                width={400}
-                height={400}
-            />
-            <div>Cena: €{((product_price.unit_amount) / 100).toFixed(2)}</div>
-            <button onClick = {() => dispatch(addToCart(product_declared.id))}>Add</button>
-
-        </div>
-        </div>
-    )
-}
 
 
 
