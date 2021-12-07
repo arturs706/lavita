30aa33f1abdddb7b061c8976f51525978698622f
arturs706 <aradionovs@yahoo.com>
2021-12-07 08:05:00 +0000

    first commit

--- pages/products/[priceId].js
+++ pages/products/[priceId].js
@@ -3,32 +3,25 @@ import Image from "next/image"
 import { useSelector, useDispatch } from 'react-redux'
 import { addToCart } from '../../redux/reducers/cartSlice'
 
-const Retrieveid = ({ product_price }) => {
-    const counter = useSelector((state) => state.counter)
-    const dispatch = useDispatch();
-    const product_declared  = product_price.product;
 
-    return (
-        
-        <div className="h-screen">
-        <div className="flex flex-col items-center">
-            <div>{product_declared.name}</div>
-            
-            <Image
-                src={product_declared.images[0]}
-                alt={product_declared.name}
-                width={400}
-                height={400}
-            />
-            <div>Cena: €{((product_price.unit_amount) / 100).toFixed(2)}</div>
-            <button onClick = {() => dispatch(addToCart(product_declared.id))}>Add</button>
 
-        </div>
-        </div>
-    )
+export async function getStaticProps(context){
+    const { params } = context
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+        apiVersion: '2020-08-27'
+    });
+    const prices = await stripe.prices.list({
+        active: true,
+      });
+    const paramsPrice = `${params.priceId}`
+    let product_price = prices.data.find(o => o.id === paramsPrice);
+return {
+    props: {
+        product_price
+    },
+    revalidate: 10
 }
-
-export default Retrieveid
+}
 
 export async function getStaticPaths(){
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
@@ -52,21 +45,44 @@ export async function getStaticPaths(){
     }
 }
 
-export async function getStaticProps(context){
-    const { params } = context
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: '2020-08-27'
-    });
-    const product_price = await stripe.prices.retrieve(
-        `${params.priceId}`,
-        {
-            expand: ['product']
-        }
-        );
-return {
-    props: {
-        product_price
-    },
-    revalidate: 10
+//getStaticProps
+
+export default function PriceId (props) { 
+    console.log(props)
+    return (
+        <div>{}</div>
+    )
 }
-}
+
+
+
+
+// export default function PriceId ({ product_price }) { 
+    
+//     const counter = useSelector((state) => state.counter)
+//     const dispatch = useDispatch();
+//     const product_declared  = product_price.product;
+
+//     return (
+        
+//         <div className="h-screen">
+//         <div className="flex flex-col items-center">
+//             <div>{product_declared.name}</div>
+            
+//             <Image
+//                 src={product_declared.images[0]}
+//                 alt={product_declared.name}
+//                 width={400}
+//                 height={400}
+//             />
+//             <div>Cena: €{((product_price.unit_amount) / 100).toFixed(2)}</div>
+//             <button onClick = {() => dispatch(addToCart(product_declared.id))}>Add</button>
+
+//         </div>
+//         </div>
+//     )
+// }
+
+
+
+
