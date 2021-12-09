d5cd1d4227de5d32b3c2f1df433d3b3304354d80
arturs706 <aradionovs@yahoo.com>
2021-12-09 19:52:11 +0000

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <div>
-      <div className="absolute z-20 left-1 top-24">
+      <div className="absolute z-20 left-1 top-20">
      <Image
       src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1639051831/first_page/Untitled-2_xikwzg.svg"
       alt="main_logo"
@@ -11,12 +11,12 @@ export default function Page() {
       height={140}
      />
      </div>
-     <div className="absolute z-10 opacity-80 top-28 right-5">
+     <div className="absolute z-10 top-40 right-5">
      <Image
-      src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1639075907/Test/rounded_nhjmx3.png"
+      src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1639073665/Test/wallpp_rle3y6.webp"
       alt="main_logo"
       width={340}
-      height={560}
+      height={500}
      />
      </div>
     </div>
