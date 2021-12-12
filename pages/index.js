ce16beb14df13ae2523ffae2262ace10a247aae1
arturs706 <aradionovs@yahoo.com>
2021-12-12 10:36:11 +0000

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,51 @@
 import Image from 'next/image'
+import { useRef, useEffect } from 'react';
+import { TweenMax, Power3 } from "gsap";
 
 export default function Page() {
+  let clickButton = useRef(null);
+
+  useEffect(()=>{
+    console.log(clickButton)
+  }, [])
+
   return (
-    <div>
+  <div>
      
-     <div className="px-3 flex flex-wrap-reverse w-full justify-evenly items-center h-screen">
-     <div className="flex flex-col sm:w-full md:w-3/12 justify-start items-center sm:h-96">
-     <h2 className = "text-5xl">Lavita Ventspils</h2>
-     <br/>
-      <h2 className = "text-5xl">Aizkaru veikals</h2>
-      <br/>
-      <Image
-      src = "https://res.cloudinary.com/dyvgcv5se/image/upload/v1639213224/lavita/Untitled-8_gdmwjl.svg"
-      layout = "intrinsic"
-      alt="main image"
-      width = {100}
-      height = {100}
-      />
-     </div>
-     <div className = "opacity-80 ">
-      <Image
-      src = "https://res.cloudinary.com/dyvgcv5se/image/upload/v1639180455/lavita/unspalsh_hexc2b.jpg"
-      layout = "intrinsic"
-      alt="main image"
-      width = {620}
-      height = {700}
-      />
-     </div>
+    <div className="px-3 flex flex-wrap-reverse w-full justify-evenly items-center h-screen">
+      <div className="flex flex-col sm:w-full md:w-1/3 justify-start items-center sm:h-11/12 ">
+        <h2 className = "text-5xl">Lavita Ventspils</h2>
+        <br/>
+        <h2 className = "text-5xl">Aizkaru veikals</h2>
+        <br/>
+        <div className ="cursor-pointer" ref={el =>{clickButton = el}}>
        
-     </div>
+          <Image
+            
+            src = "https://res.cloudinary.com/dyvgcv5se/image/upload/v1639213224/lavita/Untitled-8_gdmwjl.svg"
+            layout = "intrinsic"
+            alt="main image"
+            width = {100}
+            height = {100}
+          />
+        </div>
+      </div>
+
+      <div className = "opacity-80 md:w-2/3 flex justify-end">
+        <Image
+          priority
+          src = "https://res.cloudinary.com/dyvgcv5se/image/upload/v1639180455/lavita/unspalsh_hexc2b.jpg"
+          layout = "intrinsic"
+          alt="main image"
+          width = {620}
+          height = {700}
+        />
+      </div>
+       
+    </div>
      
 
-    </div>
+  </div>
   )
   }
 //1425px
