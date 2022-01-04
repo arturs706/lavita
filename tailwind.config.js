d1888c393b0a2578a4c5a6693a4280c3f60338b0
arturs706 <aradionovs@yahoo.com>
2022-01-04 15:24:11 +0000

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,17 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontSize: {
-        'xss': '0.6rem',
-        '10xl': '12rem'
-      },
       colors: {
         'brand': {
           50: '#ece8e1',
-          100: '#f5f2ed',
-          450: '#8c826b',
           900: '#4c473b',
         }
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
